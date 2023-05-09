36a0aa8c2910dcaab6c01e7d594d66d0f4b9ac07
Pavithramulu <128126638+Pavithramulu@users.noreply.github.com>
2023-05-09 15:10:54 +0530

    Add files via upload

---
+++ model.dart
@@ -0,0 +1,264 @@
+
+
+
+
+//list1
+class Model
+{
+  String? text;
+  String? image;
+
+
+  Model(this.text,this.image);
+}
+
+List lists = ModelData.map((e) => Model(e["text"],e["image"])).toList();
+
+var ModelData = [
+
+
+  {"image":"asset/Asus.jpg","text":"Asus",},
+  {"image":"asset/samsung.jpg","text":"Samsung",},
+  {"image":"asset/vivo.jpg","text":"Vivo",},
+  {"image":"asset/oppo.jpg","text":"Oppo",},
+  {"image":"asset/pogo.jpg","text":"Pogo",},
+  {"image":"asset/redmi.jpg","text":"Redmi",},
+
+
+];
+
+///model for CarouselSlider
+///
+///
+///
+class Cover
+{
+  String? text;
+  String? image;
+
+  Cover(this.text,this.image);
+}
+List abc = CoverData.map((e) => Cover(e["text"],e["image"])).toList();
+var CoverData = [
+  {"image":"asset/tv.jpg","text":"Led Tv",},
+  {"image":"asset/2.jpg","text":"Electrical Appliance",},
+  {"image":"asset/3.jpg","text":"womens store",},
+  {"image":"asset/4.jpg","text":"Kitchen",},
+  {"image":"asset/5.jpg","text":"Grocery",},
+  {"image":"asset/6.jpg","text":"Electrical Appliance",},
+
+];
+
+///ListView /////
+
+class Biger{
+  String? text;
+  String? image;
+
+  Biger(this.text,this.image);
+
+}
+List look = BigerData.map((e) => Biger(e["text"],e["image"])).toList();
+var BigerData = [
+
+  {"image":"asset/7.jpg","text":"Headset",},
+  {"image":"asset/8.jpg","text":"Straightener",},
+  {"image":"asset/9.jpg","text":"Shoes",},
+  {"image":"asset/55.jpg","text":"Tops",},
+  {"image":"asset/46.jpg","text":"Jeans",},
+
+];
+
+
+//
+class Smaller{
+  String? text;
+  String? image;
+
+  Smaller(this.text,this.image);
+
+}
+List like = SmallerData.map((e) => Smaller(e["text"],e["image"])).toList();
+var SmallerData = [
+
+ // {"image":"asset/13.jpg","text":"Longtops",},
+  {"image":"asset/14.jpg","text":"Sarees",},
+  {"image":"asset/18.jpg","text":"Office wear",},
+ // {"image":"asset/15.jpg","text":"Salwars",},
+ // {"image":"asset/16.jpg","text":"Jeantop",},
+ // {"image":"asset/17.jpg","text":"Tops",},
+  {"image":"asset/18.jpg","text":"Office wear",},
+  {"image":"asset/14.jpg","text":"Sarees",},
+
+];
+
+////
+
+
+class Headset{
+
+  String? image;
+  String? text;
+
+  Headset(this.image,this.text);
+
+}
+List headset = HeadsetData.map((e) => Headset(e["image"],e["text"])).toList();
+var HeadsetData = [
+
+  {"image":"asset/h0.jpg","text":"Luscious Black"},
+  {"image":"asset/b7.jpg","text":"Aua Blue"},
+  {"image":"asset/b6.jpg","text":"Hazel Beige"},
+  {"image":"asset/b5.jpg","text":"Knight Black"},
+  {"image":"asset/b3.jpg","text":"Soldiers Blue"},
+
+
+];
+
+//
+
+class Headsets{
+
+  String? image;
+  String? text;
+
+  Headsets(this.image,this.text);
+
+}
+List headset1 = HeadsetsData.map((e) => Headsets(e["image"],e["text"])).toList();
+
+
+var HeadsetsData = [
+
+  {"image":"asset/h0.jpg","text":"Luscious Black"},
+  {"image":"asset/hh1.jpg","text":"Aua Blue"},
+  {"image":"asset/hh2.jpg","text":"Hazel Beige"},
+  {"image":"asset/hh3.jpg","text":"Knight Black"},
+  {"image":"asset/hh4.jpg","text":" Soldiers Blue"},
+  {"image":"asset/hhh5.jpg","text":"A"},
+  {"image":"asset/hhh6.jpg","text":"A"},
+
+
+];
+//
+
+class Head{
+
+  String? text;
+
+  Head(this.text);
+
+}
+List type = typeData.map((e) => Head(e["text"])).toList();
+var typeData = [
+
+  {"text":"No Cost EMI : Avall No Cost EMI on select cards for orders above 3000",},
+  {"text":"Cash Back : Upto 100 caseback & 500 welcome rewards on Amazon Pay Later",},
+  {"text":"Bank Offer : Upto 89.92 discount on select Credit cards",},
+  {"text":"Partner Offers : Get GST invoice and save up to 28% on business purchase",},
+
+
+];//
+
+class Govt{
+
+  String? image;
+
+  Govt(this.image);
+
+}
+List ddd = GovtData.map((e) => Govt(e["image"])).toList();
+
+
+var GovtData = [
+
+  {"image":"asset/a.jpg"},
+  {"image":"asset/b.jpg"},
+  {"image":"asset/c.jpg"},
+  {"image":"asset/d.jpg"},
+  {"image":"asset/e.jpg"},
+  {"image":"asset/f.jpg"},
+  {"image":"asset/g.jpg"},
+  {"image":"asset/h.jpg"},
+  {"image":"asset/i.jpg"},
+  {"image":"asset/j.jpg"},
+
+];
+
+class Govt1{
+
+  String? image;
+
+  Govt1(this.image);
+
+}
+List man = Govt1Data.map((e) => Govt1(e["image"])).toList();
+
+
+var Govt1Data = [
+
+  {"image":"asset/k.jpg"},
+  {"image":"asset/l.jpg"},
+  {"image":"asset/m.jpg"},
+  {"image":"asset/n.jpg"},
+  {"image":"asset/o.jpg"},
+  {"image":"asset/p.jpg"},
+  {"image":"asset/q.jpg"},
+  {"image":"asset/r.jpg"},
+  {"image":"asset/s.jpg"},
+  {"image":"asset/t.jpg"},
+
+];//
+
+class calsi
+{
+  String? text;
+
+
+
+  calsi(this.text,);
+}
+
+List calculate = calsiData.map((e) => calsi(e["text"])).toList();
+
+var calsiData = [
+
+
+  {"text":"1*10",},
+  {"text":"2*20",},
+  {"text":"3*30",},
+  {"text":"4*40",},
+  {"text":"5*50",},
+  {"text":"6*60",},
+  {"text":"7*70",},
+  {"text":"8*80",},
+  {"text":"9*90",},
+  {"text":"10*100",},
+  {"text":"11*110",},
+  {"text":"12*120",},
+  {"text":"13*130",},
+  {"text":"14*140",},
+  {"text":"15*150",},
+  {"text":"16*160",},
+  {"text":"17*170",},
+  {"text":"18*180",},
+  {"text":"19*190",},
+  {"text":"20*200",},
+  {"text":"21*210",},
+  {"text":"22*220",},
+  {"text":"23*230",},
+  {"text":"24*240",},
+  {"text":"25*250",},
+
+
+
+];
+
+
+
+
+
+
+
+
+
